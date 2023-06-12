340a565e27203d128540898aee62d600bad257a2
beboopr <linnets-router.09@icloud.com>
2023-06-12 01:47:30 -0400

    imported react-icons

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
 interface SidebarProps {
     children: React.ReactNode;
 }
@@ -5,6 +11,23 @@ interface SidebarProps {
 const Sidebar: React.FC<SidebarProps> = ({
     children
 }) => {
+    const pathname = usePathname();
+
+    const routes = useMemo(() => [
+        {   
+            icon: HiHome,
+            label: 'Home',
+            active: pathname !== '/search',
+            href: '/',
+        },
+        {   
+            icon: BiSearch,
+            label: 'Search',
+            active: pathname === '/search',
+            href: '/search',
+        }
+    ], [pathname]);
+
   return (
     <div>{children}</div>
   );
